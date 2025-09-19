b484031d8939b7286a7c19708d9c0884260c7ad1
ashish arora <ashisharora24@outlook.com>
2025-09-19 12:46:10 +0530

    v2

--- function_app.py
+++ function_app.py
@@ -1,4 +1,13 @@
+import logging, os, json
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+
+SB_NAMESPACE = os.environ["SERVICE_BUS_NAMESPACE"]     # e.g., sb-poc-core
+SB_FQDN = f"{SB_NAMESPACE}.servicebus.windows.net"
+QUEUE_NAME = os.environ.get("SERVICE_BUS_QUEUE_NAME", "q-work")
+
+credential = DefaultAzureCredential()
 
 # Create the app object
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -6,6 +15,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.function_name(name="hello")
 @app.route(route="hello", methods=["GET", "POST"])
 def hello(req: func.HttpRequest) -> func.HttpResponse:
+
+    logging.info("v2")
+
     name = req.params.get("name")
     if not name:
         try:
